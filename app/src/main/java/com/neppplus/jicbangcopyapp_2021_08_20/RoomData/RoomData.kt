f51e42c417e007f2037785e8e50e89d15133d514
유병재 <dqwdd@naver.com>
2021-08-20 17:59:05 +0900

    RoomData 추가

--- app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/RoomData/RoomData.kt
+++ app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/RoomData/RoomData.kt
@@ -3,6 +3,6 @@ package com.neppplus.jicbangcopyapp_2021_08_20.RoomData
 class RoomData (
     val place : String,
     val price : Int,
-    val floar : Int,
-    val explain : String
-) { }
+    val floor : Int,
+    val description : String
+)
