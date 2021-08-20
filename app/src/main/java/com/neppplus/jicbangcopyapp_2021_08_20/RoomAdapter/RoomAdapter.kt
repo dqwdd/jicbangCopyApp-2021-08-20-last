fa783a86bb69f31dfa453b6e00cea09d05015ef7
유병재 <dqwdd@naver.com>
2021-08-20 18:21:57 +0900

    일단 한거 저장

--- app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/RoomAdapter/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/RoomAdapter/RoomAdapter.kt
@@ -1,8 +1,48 @@
 package com.neppplus.jicbangcopyapp_2021_08_20.RoomAdapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.neppplus.jicbangcopyapp_2021_08_20.R
+import com.neppplus.jicbangcopyapp_2021_08_20.RoomData.RoomData
+
 class RoomAdapter (
-    val address : String,
-    val price : Int,
-    val floor : Int,
-    val description : String
-        )
+    val mContext : Context,
+    val resId : Int,
+    val mroomList : ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mroomList){
+
+        val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow==null) {
+            tempRow = mInflater.inflate(R.layout.room_item_list, null)
+        }
+        val row = tempRow!!
+
+        val data = mroomList[position]
+
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val placeTxt = row.findViewById<TextView>(R.id.placeTxt)
+        val floorTxt = row.findViewById<TextView>(R.id.floorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+//
+//
+        if (data.floor >= 1)
+        priceTxt.text = data.price.toString()
+        placeTxt.text = data.place
+        floorTxt.text = data.price.toString()
+        descriptionTxt.text = data.price.toString()
+
+
+        return row
+    }
+
+    }
+
+
