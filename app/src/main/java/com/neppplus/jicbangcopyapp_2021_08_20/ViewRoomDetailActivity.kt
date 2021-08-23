9c8d9e18fb930633258dca5d95c8488788be6986
keemaan <keemaan@naver.com>
2021-08-23 10:42:53 +0900

    방 상세 화면 추가 중 (UI작업) 중간 커밋

---
+++ app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/ViewRoomDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.neppplus.jicbangcopyapp_2021_08_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+
+
+
+    }
+}
