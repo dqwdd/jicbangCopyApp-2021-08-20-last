620816a5f5cd72feb4ccfe4cf372339d98e00367
keemaan <keemaan@naver.com>
2021-08-23 11:59:25 +0900

    앱의 시각 정보 활용 설정 앱 시작 화면용 SplashActivity 추가

---
+++ app/src/main/java/com/neppplus/jicbangcopyapp_2021_08_20/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.jicbangcopyapp_2021_08_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
